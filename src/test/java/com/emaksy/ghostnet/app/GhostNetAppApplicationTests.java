6ca9b11322bd510232bd3b41e89b7289b1a2b4b1
EMaksy <emaksymenko@suse.de>
2025-12-13 22:39:11 +0100

    Fix broken test

--- src/test/java/com/emaksy/ghostnet/app/GhostNetAppApplicationTests.java
+++ src/test/java/com/emaksy/ghostnet/app/GhostNetAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.emaksy.ghostnet.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GhostNetAppApplicationTests {
 
   @Test
